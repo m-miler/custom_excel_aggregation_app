1dcf045906b240b54fcf376c6bf138d3fc6b9995
m-miler <mmiler30@gmail.com>
2023-02-09 17:09:35 +0100

    fix: change settings folder path

--- custom_excel_aggregation_app/utils.py
+++ custom_excel_aggregation_app/utils.py
@@ -13,14 +13,14 @@ class Utils:
     def save_settings_to_json_file(settings: dict) -> None:
         """ Save survey settings as json file."""
         settings_file_name = f"settings_{settings.get('SURVEY_NAME')}"
-        with open(f'colostrum_code/survey_settings/{settings_file_name}.json', 'w', encoding='utf-8') as file:
+        with open(f'custom_excel_aggregation_app/survey_settings/{settings_file_name}.json', 'w', encoding='utf-8') as file:
             json.dump(settings, file,  ensure_ascii=False, indent=2)
             print('Survey has been created correctly')
 
     @staticmethod
     def check_if_setting_file_exist(survey_name: str) -> dict | None:
         """ Check if settings for chosen survey exists. If true return survey settings."""
-        settings_file_path = f"colostrum_code/survey_settings/settings_{survey_name}.json"
+        settings_file_path = f"custom_excel_aggregation_app/survey_settings/settings_{survey_name}.json"
         if os.path.isfile(settings_file_path):
             with open(settings_file_path, 'r', encoding='utf-8') as file:
                 settings = json.load(file)
